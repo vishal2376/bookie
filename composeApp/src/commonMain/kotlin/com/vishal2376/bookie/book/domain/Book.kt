5a65fce51e073ce11a33987eb87f801bec39d62a
vishal2376 <vishalsingh2376@gmail.com>
2024-11-23 23:35:25 +0530

    feat: Add Book Model

---
+++ composeApp/src/commonMain/kotlin/com/vishal2376/bookie/book/domain/Book.kt
@@ -0,0 +1,15 @@
+package com.vishal2376.bookie.book.domain
+
+data class Book(
+	val id: String,
+	val title: String,
+	val imageUrl: String,
+	val authors: List<String>,
+	val description: String?,
+	val language: String,
+	val firstPublisher: String?,
+	val averageRating: Double?,
+	val ratingCount: Int?,
+	val numPages: Int?,
+	val numEditions: Int,
+)
