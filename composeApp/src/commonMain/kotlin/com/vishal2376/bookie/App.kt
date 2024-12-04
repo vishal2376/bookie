5073982ca0dcb07ac84e930a35ea9ba8c45195aa
vishal2376 <vishalsingh2376@gmail.com>
2024-12-04 21:04:05 +0530

    chore: Add BookListScreen

--- composeApp/src/commonMain/kotlin/com/vishal2376/bookie/App.kt
+++ composeApp/src/commonMain/kotlin/com/vishal2376/bookie/App.kt
@@ -1,27 +1,14 @@
 package com.vishal2376.bookie
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import com.vishal2376.bookie.book.presentation.book_list.BookListScreenRoot
+import com.vishal2376.bookie.book.presentation.book_list.BookListViewModel
 import com.vishal2376.bookie.core.presentation.theme.BookieTheme
 
 @Composable
 fun App(darkTheme: Boolean, dynamicColor: Boolean) {
 	BookieTheme(darkTheme, dynamicColor) {
-		Scaffold { innerPadding ->
-			Column(
-				modifier = Modifier.fillMaxSize().padding(innerPadding),
-				verticalArrangement = Arrangement.Center,
-				horizontalAlignment = Alignment.CenterHorizontally
-			) {
-				Text("Hello World")
-			}
-		}
+		BookListScreenRoot(viewModel = remember { BookListViewModel() }, onBookClick = {})
 	}
 }
