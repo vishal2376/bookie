5392ed9601ea05f4c780c37eccb5255df2a32114
vishal2376 <vishalsingh2376@gmail.com>
2024-12-22 20:55:41 +0530

    chore: update book detail actions

--- composeApp/src/commonMain/kotlin/com/vishal2376/bookie/book/presentation/book_detail/BookDetailAction.kt
+++ composeApp/src/commonMain/kotlin/com/vishal2376/bookie/book/presentation/book_detail/BookDetailAction.kt
@@ -1,6 +1,9 @@
 package com.vishal2376.bookie.book.presentation.book_detail
 
+import com.vishal2376.bookie.book.domain.Book
+
 sealed interface BookDetailAction {
 	data object OnClickBack : BookDetailAction
 	data object OnClickFavorite : BookDetailAction
+	data class OnSelectedBookChanged(val book: Book) : BookDetailAction
 }
