82089e7a53d5f39349919a4a2efd2c092d21b83b
Vishal Singh <vishalsingh2376@gmail.com>
2025-03-11 16:48:29 +0530

    fix: ios database build issue

--- composeApp/src/commonMain/kotlin/com/vishal2376/bookie/book/data/database/FavoriteBookDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/vishal2376/bookie/book/data/database/FavoriteBookDatabase.kt
@@ -1,5 +1,6 @@
 package com.vishal2376.bookie.book.data.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
@@ -10,10 +11,11 @@ import com.vishal2376.bookie.book.data.database.converters.StringTypeConverter
 	version = 1
 )
 @TypeConverters(StringTypeConverter::class)
+@ConstructedBy(DatabaseConstructor::class)
 abstract class FavoriteBookDatabase : RoomDatabase() {
 	abstract val favoriteBookDao: FavoriteBookDao
 
 	companion object {
-		const val DATABASE_NAME = "favorite_books_db"
+		const val DATABASE_NAME = "favorite_books.db"
 	}
 }
